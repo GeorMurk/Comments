cc64b073d58b23b3cb5ff0a833e0394714ace1ad
George Kiragu <45111839+GeorMurk@users.noreply.github.com>
2019-02-02 01:41:31 +0300

    Add files via upload

---
+++ Comments.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+#!Creating Comments
+
+print("Everything I've been writing and they seem grayish in color those are comments on the code reserved for the coder."
+      " And they're usually hashed out using the hash symbol(#) then everything followed will not be rendered out by the python console.")
+
+
+
+#!Go ahead and test it out!!!!
